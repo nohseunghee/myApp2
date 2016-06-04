30a5061ae65dd14a9e5d37e302ea7673f0fc5c2c
nohseunghee <rshnar@hanmail.net>
2016-06-04 11:11:20 +0900

    2016-06-04 작업테스트2

--- app/src/main/java/com/hanbit/user/myapp2/MainActivity.java
+++ app/src/main/java/com/hanbit/user/myapp2/MainActivity.java
@@ -14,57 +14,61 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-    EditText weight;
-    EditText height;
-    EditText name;
+
+    EditText etWeight, etHeight, etName;
     TextView resultCalc;
     Button btnCalc;
-    String resultMsg;
+
+    String name, resultText;
+    double weight, height;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        weight = (EditText)findViewById(R.id.weight);
-        height = (EditText)findViewById(R.id.height);
-        name = (EditText)findViewById(R.id.name);
+        etWeight = (EditText)findViewById(R.id.weight);
+        etHeight = (EditText)findViewById(R.id.height);
+        etName = (EditText)findViewById(R.id.name);
         resultCalc = (TextView)findViewById(R.id.resultCalc);
         btnCalc = (Button)findViewById(R.id.btnCalc);
+
+
 
 
 
         btnCalc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
+                name = etName.getText().toString();
+                height = Double.parseDouble(etHeight.getText().toString());
+                weight = Double.parseDouble(etWeight.getText().toString());
+                //Integer.parseInt(~~~~) :: 정수 casting
+
+
                 //공식
                 //몸무게를 키의 제곱으로 나눈후, 10000을 곱한 인덱스 값
                 //int idx = (int)(weight/(height * height)) * 10000;
-
-                int nheight = Integer.parseInt(height.getText().toString());
-                int nweight = Integer.parseInt(weight.getText().toString());
-
-                int idx = (int)(nweight/(nheight * nheight)) * 10000;
+                int idx = (int)(weight/(height * height)) * 10000;
 
 
                 if(idx >= 30){ //비만
-                    resultMsg = "비만";
+                    resultText = "비만";
                 }else if(idx >= 24){ //과체중
-                    resultMsg = "과체중";
+                    resultText = "과체중";
                 }else if(idx >= 20){ //정상
-                    resultMsg = "정상";
+                    resultText = "정상";
                 }else if(idx >= 15){ //저체중
-                    resultMsg = "저체중";
+                    resultText = "저체중";
                 }else if(idx >= 13){ //마름
-                    resultMsg = "마름";
+                    resultText = "마름";
                 }else if(idx >= 10){ //영양실조
-                    resultMsg = "영양실조";
+                    resultText = "영양실조";
                 }else{//소모증
-                    resultMsg = "소모증";
+                    resultText = "소모증";
                 }
-                resultCalc.setText(name.getText()+"님의 계산결과: " + resultMsg);
-
+                resultCalc.setText(name+"님의 계산결과: " + resultText);
             }
         });
 
