9354f173b9ec4bee319dc12f34d98c134b9ba391
nohseunghee <rshnar@hanmail.net>
2016-06-18 14:41:11 +0900

    2016-06-18 작업2

--- app/src/main/java/com/hanbit/user/myapp2/group/GroupActivity.java
+++ app/src/main/java/com/hanbit/user/myapp2/group/GroupActivity.java
@@ -2,9 +2,11 @@ package com.hanbit.user.myapp2.group;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,6 +21,8 @@ public class GroupActivity extends Activity implements View.OnClickListener{
     TextView txtResult;
     GroupDBHelper ghelper;
     SQLiteDatabase db;
+
+    public String TAG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,64 +45,101 @@ public class GroupActivity extends Activity implements View.OnClickListener{
 
     @Override
     public void onClick(View v) {
+
+
         if(v.getId() == R.id.btnMain){    //메인으로
             startActivity(new Intent(this, MainActivity.class));
             return;
         }
+
         String resultMsg = "";
-
-
-        GroupBean gbean = new GroupBean();  //인스턴스(객체) 생성
-        gbean.setName(etName.getText().toString());
-        gbean.setCnt(Integer.parseInt(etCnt.getText().toString()));
-
         ghelper    = new GroupDBHelper(this);
+        Cursor cursor = null;
+        GroupBean gbean = new GroupBean();  //인스턴스(객체) 생성
+
+        String strId = "번호\r\n----\r\n";
+        String strNames = "그룹이름\r\n----\r\n";
+        String strNumbers = "인원\r\n----\r\n";
 
 
-        //GroupService service = new GroupServiceImpl();
-        /*
 
-        member = dao.login(member);
-        * */
-        switch(v.getId()){
+        switch(v.getId()) {
             case R.id.btnInit://초기화
+                db = ghelper.getWritableDatabase();
                 ghelper.onUpgrade(db,1,2);
                 break;
             case R.id.btnCount:   //그룹수
+
                 resultMsg = "그룹수";
                 db = ghelper.getReadableDatabase();
-                db.rawQuery("select count(_id) from group   ",null);
+                cursor = db.rawQuery("select * from girl_group   ",null);
+                //txtResult.setText("그룹수: "+cursor.getInt(0));
+                txtResult.setText("그룹수: "+cursor.getCount());
+
+                cursor.close();
                 break;
             case R.id.btnlist:  //리스트
                 resultMsg = "리스트";
                 db = ghelper.getReadableDatabase();
-                db.rawQuery("select * from group ",null);
-                String strNames = "그룹이름:"+gbean.getName()+"----\r\n";
-                String strNumbers = "인원:"+gbean.getCnt()+"----\r\n";
-                break;
-            case R.id.btnInsert://입력
-                resultMsg = "입력됨";
-                db = ghelper.getWritableDatabase();
-                db.execSQL("insert into group (name,num) values (IOI','20')");
+                cursor = db.rawQuery("select * from girl_group ",null);
+
+                while(cursor.moveToNext()){
+                    strId += cursor.getInt(0)+"\r\n";
+                    strNames += cursor.getString(1)+"\r\n";
+                    strNumbers += cursor.getInt(2)+"\r\n";
+                }
+
+                txtResult.setText(strNames);
+                cursor.close();
                 break;
             case R.id.btnSearch://조회
+                gbean.setName(etName.getText().toString());
+                gbean.setCnt(Integer.parseInt(etCnt.getText().toString()));
+
                 resultMsg = "조회됨";
                 db = ghelper.getReadableDatabase();
-                db.rawQuery("select * from group where name='IOI' limit 10 ",null);
+                cursor = db.rawQuery("select * from girl_group where name='"+gbean.getName()+"' and num='"+gbean.getCnt()+"' ",null);
+
+                cursor.moveToNext();
+                //cursor.moveToFirst();
+                strId += String.valueOf(cursor.getInt(0));
+                strNames += cursor.getString(1);
+                strNumbers += String.valueOf(cursor.getInt(2));
+
+                txtResult.setText(strNames);
+                cursor.close();
                 break;
+            case R.id.btnInsert://입력
+
+
+                gbean.setName(etName.getText().toString());
+                gbean.setCnt(Integer.parseInt(etCnt.getText().toString()));
+
+                resultMsg = "입력됨";
+                db = ghelper.getWritableDatabase();
+                db.execSQL("insert into girl_group (name,num) values ('"+gbean.getName()+"','"+gbean.getCnt()+"')");
+
+                break;
+
             case R.id.btnMod://수정
+                gbean.setName(etName.getText().toString());
+                gbean.setCnt(Integer.parseInt(etCnt.getText().toString()));
+
                 resultMsg = "수정됨";
                 db = ghelper.getWritableDatabase();
-                db.execSQL("update group set name='ttt', num='333' ");
+                db.execSQL("update girl_group set num='"+gbean.getCnt()+"' where _id='1' ");
                 break;
             case R.id.btnDel://삭제
                 resultMsg = "삭제됨";
                 db = ghelper.getWritableDatabase();
-                db.execSQL("delete from group where _id = '1' ");
+                db.execSQL("delete from girl_group where _id = '10' ");
+
                 break;
         }
         db.close();
-
         Toast.makeText(getApplicationContext(),resultMsg, Toast.LENGTH_LONG).show();
+        return;
+
+
     }
 }
--- app/src/main/java/com/hanbit/user/myapp2/group/GroupDBHelper.java
+++ app/src/main/java/com/hanbit/user/myapp2/group/GroupDBHelper.java
@@ -3,6 +3,8 @@ package com.hanbit.user.myapp2.group;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by USER on 2016-06-18.
@@ -11,18 +13,22 @@ public class GroupDBHelper extends SQLiteOpenHelper{
     private static final String DB_NAME = "hanbitDB";
     private static final int DB_Version = 1;
 
+    public String TAG;
+
     public GroupDBHelper(Context context) {
         super(context, DB_NAME, null, DB_Version);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table group (_id integer primary key, name text, num integer");
+        Log.i(TAG, "ok2");
+        db.execSQL("create table girl_group (_id integer primary key, name text, num integer);");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("drop table if exists group");
+        Log.i(TAG, "ok1");
+        db.execSQL("drop table if exists girl_group");
         this.onCreate(db);;
     }
 }
